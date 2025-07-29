b2a712efd443e566f1a42428b8164afe031e7891
213299298 <25sekhwelad@gmail.com>
2025-07-29 22:38:49 +0200

    Add DB Context

---
+++ ContactBook/src/ContactBook.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ContactBook.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace AddressBook.Infrastructure;
+
+public sealed class ApplicationDbContext : DbContext, IUnitOfwork
+{
+    public ApplicationDbContext(DbContextOptions options)
+          : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+}
---
+++ ContactBook/src/ContactBook.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,25 @@
+﻿using ContactBook.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace AddressBook.Infrastructure.Repositories;
+
+internal abstract class Repository<T> where T : Entity
+{
+    protected readonly ApplicationDbContext _dbContext;
+
+    protected Repository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        return await _dbContext.Set<T>()
+            .FirstOrDefaultAsync(user => user.Id == id, cancellationToken);
+    }
+
+    public virtual void Add(T entity)
+    {
+        _dbContext.Add(entity);
+    }
+}
