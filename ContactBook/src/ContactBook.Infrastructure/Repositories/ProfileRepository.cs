71a4fd15b1c9656e2ebae635511533e0eb9300f2
213299298 <25sekhwelad@gmail.com>
2025-07-29 22:56:25 +0200

    Add Profile Repository

---
+++ ContactBook/src/ContactBook.Infrastructure/Repositories/ProfileRepository.cs
@@ -0,0 +1,12 @@
+﻿using ContactBook.Domain.Persons;
+
+namespace AddressBook.Infrastructure.Repositories;
+
+internal sealed class ProfileRepository : Repository<Profile>, IProfileRepository
+{
+    public ProfileRepository(ApplicationDbContext dbContext)
+        : base(dbContext)
+    {
+        
+    }
+}
