befafd0e0f96a15118cf3e60fb6b12ef326c8b4e
213299298 <25sekhwelad@gmail.com>
2025-07-29 23:09:52 +0200

    Configure Dependency Injection

--- ContactBook/src/ContactBook.Infrastructure/Configurations/ProfileConfiguration.cs
+++ ContactBook/src/ContactBook.Infrastructure/Configurations/ProfileConfiguration.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace AddressBook.Infrastructure;
+namespace AddressBook.Infrastructure.Configurations;
 
 internal sealed class ProfileConfiguration : IEntityTypeConfiguration<Profile>
 {
---
+++ ContactBook/src/ContactBook.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,37 @@
+﻿using AddressBook.Application.Abstractions.Data;
+using AddressBook.Infrastructure.Data;
+using AddressBook.Infrastructure.Repositories;
+using ContactBook.Domain.Abstractions;
+using ContactBook.Domain.Persons;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AddressBook.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        AddPersistence(services, configuration);
+
+        return services;
+    }
+    public static void AddPersistence(IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Database") ??
+                throw new ArgumentNullException(nameof(configuration));
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
+
+        services.AddScoped<IProfileRepository, ProfileRepository>();
+        services.AddScoped<IUnitOfwork>(sp => sp.GetRequiredService<ApplicationDbContext>());
+
+        services.AddSingleton<ISqlConnectionFactory>(_ =>
+        new SqlConnectionFactory(connectionString));
+    }
+
+}
