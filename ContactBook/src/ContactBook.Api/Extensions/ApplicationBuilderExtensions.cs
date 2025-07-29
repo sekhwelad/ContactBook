889c21c0f184269c4e583f5755be5046ea1248d4
213299298 <25sekhwelad@gmail.com>
2025-07-29 23:20:20 +0200

    Configure Dependency Injection

---
+++ ContactBook/src/ContactBook.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using AddressBook.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace AddressBook.Api.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+
+        using var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        dbContext.Database.Migrate();
+    }
+}
---
+++ ContactBook/src/ContactBook.Api/Extensions/SeedDataExtensions.cs
@@ -0,0 +1,35 @@
+﻿using AddressBook.Application.Abstractions.Data;
+using Bogus;
+
+namespace AddressBook.Api.Extensions;
+
+public static class SeedDataExtensions
+{
+    public static void SeedData(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+
+        var sqlConnectionFactory = scope.ServiceProvider.GetRequiredService<ISqlConnectionFactory>();
+        using var connection = sqlConnectionFactory.CreateConnection();
+
+        var faker = new Faker();
+
+        List<object> profiles = new();
+
+        for (int x= 0; x<10; x++)
+        {
+            profiles.Add(
+                new
+                {
+                    Id = Guid.NewGuid(),
+                    FirstName = faker.Person.FirstName,
+                    LastName = faker.Person.LastName,
+                    Description = "I am just another person",
+                    Email = faker.Person.Email,
+                    Cellphone = faker.Person.Phone,
+                    Website = $"{faker.Person.FullName}.co.za"
+                });
+        }
+    }
+}
+
