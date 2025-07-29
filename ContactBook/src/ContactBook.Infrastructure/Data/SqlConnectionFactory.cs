d20cd488fb6b56708f5e207df1ea277c90f85e5c
213299298 <25sekhwelad@gmail.com>
2025-07-29 22:41:04 +0200

    Add Sql Connection Factory

---
+++ ContactBook/src/ContactBook.Infrastructure/Data/SqlConnectionFactory.cs
@@ -0,0 +1,22 @@
+﻿using AddressBook.Application.Abstractions.Data;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace AddressBook.Infrastructure.Data;
+
+internal sealed class SqlConnectionFactory : ISqlConnectionFactory
+{
+    private readonly string _connectionString;
+
+    public SqlConnectionFactory(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public IDbConnection CreateConnection()
+    {
+        var connection = new SqlConnection(_connectionString);
+        connection.Open();
+        return connection;
+    }
+}
